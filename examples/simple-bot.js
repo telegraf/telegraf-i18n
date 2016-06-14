697e9481ee55bd10d9f9b1781e66d4cd8341b356
Vitaly Domnikov <dotcypress@gmail.com>
2016-06-13 23:25:41 -0700

    updete to Telegraf 2.0

--- examples/simple-bot.js
+++ examples/simple-bot.js
@@ -13,40 +13,40 @@ telegraf.use(i18n({
 }))
 
 // Start bot
-telegraf.on('/start', function * () {
-  var message = this.i18n.t('greeting', {
-    username: this.from.username
+telegraf.hears('/start', (ctx) => {
+  var message = ctx.i18n.t('greeting', {
+    username: ctx.from.username
   })
-  yield this.reply(message)
+  return ctx.reply(message)
 })
 
 // Set locale to `en`
-telegraf.hears('/en', function * () {
-  this.i18n.locale('en')
+telegraf.hears('/en', (ctx) => {
+  ctx.i18n.locale('en')
 })
 
 // Set locale to `ru`
-telegraf.hears('/ru', function * () {
-  this.i18n.locale('ru')
+telegraf.hears('/ru', (ctx) => {
+  ctx.i18n.locale('ru')
 })
 
 // Add apple to cart
-telegraf.hears('/add', function * () {
-  this.session.appleCount = this.session.appleCount || 0
-  this.session.appleCount++
-  var message = this.i18n.t('cart', {appleCount: this.session.appleCount})
-  yield this.reply(message)
+telegraf.hears('/add', (ctx) => {
+  ctx.session.appleCount = ctx.session.appleCount || 0
+  ctx.session.appleCount++
+  var message = ctx.i18n.t('cart', {appleCount: ctx.session.appleCount})
+  return ctx.reply(message)
 })
 
 // Add apple to cart
-telegraf.hears('/cart', function * () {
-  var message = this.i18n.t('cart', {appleCount: this.session.appleCount || 0})
-  yield this.reply(message)
+telegraf.hears('/cart', (ctx) => {
+  var message = ctx.i18n.t('cart', {appleCount: ctx.session.appleCount || 0})
+  return ctx.reply(message)
 })
 
 // Random joke
-telegraf.hears('/joke', function * () {
-  yield this.reply(this.i18n.t('joke'))
+telegraf.hears('/joke', (ctx) => {
+  return ctx.reply(ctx.i18n.t('joke'))
 })
 
 telegraf.startPolling()
