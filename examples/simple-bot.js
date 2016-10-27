77d9a72561c62b03b37e3a9c9c7337c0872bdf5e
Vitaly Domnikov <dotcypress@gmail.com>
2016-10-26 20:36:04 -0700

    cleanup

--- examples/simple-bot.js
+++ examples/simple-bot.js
@@ -2,20 +2,20 @@ const Telegraf = require('telegraf')
 const path = require('path')
 const I18n = require('../lib/i18n')
 
-const telegraf = new Telegraf(process.env.BOT_TOKEN)
+const app = new Telegraf(process.env.BOT_TOKEN)
 
 // For testing only. Information about current locale will be lost on app restart
-telegraf.use(Telegraf.memorySession())
+app.use(Telegraf.memorySession())
 
 // Add middleware
 const i18n = new I18n({
   directory: path.resolve(__dirname, 'locales')
 })
 
-telegraf.use(i18n.middleware())
+app.use(i18n.middleware())
 
 // Start bot
-telegraf.command('/start', (ctx) => {
+app.command('/start', (ctx) => {
   const message = ctx.i18n.t('greeting', {
     username: ctx.from.username
   })
@@ -23,17 +23,13 @@ telegraf.command('/start', (ctx) => {
 })
 
 // Set locale to `en`
-telegraf.command('/en', (ctx) => {
-  ctx.i18n.locale('en')
-})
+app.command('/en', (ctx) => ctx.i18n.locale('en'))
 
 // Set locale to `ru`
-telegraf.command('/ru', (ctx) => {
-  ctx.i18n.locale('ru')
-})
+app.command('/ru', (ctx) => ctx.i18n.locale('ru'))
 
 // Add apple to cart
-telegraf.command('/add', (ctx) => {
+app.command('/add', (ctx) => {
   ctx.session.appleCount = ctx.session.appleCount || 0
   ctx.session.appleCount++
   const message = ctx.i18n.t('cart', {appleCount: ctx.session.appleCount})
@@ -41,9 +37,9 @@ telegraf.command('/add', (ctx) => {
 })
 
 // Add apple to cart
-telegraf.command('/cart', (ctx) => {
+app.command('/cart', (ctx) => {
   const message = ctx.i18n.t('cart', {appleCount: ctx.session.appleCount || 0})
   return ctx.reply(message)
 })
 
-telegraf.startPolling(60)
+app.startPolling()
