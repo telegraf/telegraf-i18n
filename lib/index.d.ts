52a506d5ab20974b22c742110bbd0a0d6c2bd2b2
farm <lukas_karotta@hotmail.com>
2018-08-23 15:42:16 -0300

    Adding TS typings that I've been using it.

---
+++ lib/index.d.ts
@@ -0,0 +1,25 @@
+declare module 'telegraf-i18n' {
+    interface Config {
+        directory: string;
+        useSession: boolean;
+        sessionName: string;
+        allowMissing: boolean;
+        defaultLanguage: string;
+    }
+
+    type ContextUpdate = (ctx: any, next?: (() => any) | undefined) => any;
+
+    export class I18n {
+        constructor (input: Config);
+        loadLocales (directory: string): void;
+        loadLocale (languageCode: string, i18Data: object): void;
+        resetLocale (languageCode: string): void;
+        middleware(): ContextUpdate;
+        createContext (languageCode: string, templateData: object): void;
+        t (languageCode?: string, resourceKey?: string, templateData?: object): string;
+        t (resourceKey?: string, templateData?: object): string;
+        locale (): string;
+    }
+
+    export default I18n;
+}
