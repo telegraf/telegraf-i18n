174ef63ffd739e4678569b656af6c265909cce46
Vitaly Domnikov <dotcypress@gmail.com>
2019-08-16 20:12:02 -0700

    restore createContrext

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -21,6 +21,7 @@ declare module 'telegraf-i18n' {
         overspecifiedKeys (languageOfInterest: string, referenceLanguage?: string): string[];
         translationProgress (languageOfInterest: string, referenceLanguage?: string): number;
         middleware(): ContextUpdate;
+        createContext (languageCode: string, templateData: object): void;
         t (languageCode?: string, resourceKey?: string, templateData?: object): string;
         t (resourceKey?: string, templateData?: object): string;
         locale (): string;
