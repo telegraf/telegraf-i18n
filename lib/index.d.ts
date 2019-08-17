cae4c13db92d92564349c605ccfd64f9b3429e52
Vitaly Domnikov <dotcypress@gmail.com>
2019-08-16 18:36:32 -0700

    custom template functions. #15

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -21,7 +21,6 @@ declare module 'telegraf-i18n' {
         overspecifiedKeys (languageOfInterest: string, referenceLanguage?: string): string[];
         translationProgress (languageOfInterest: string, referenceLanguage?: string): number;
         middleware(): ContextUpdate;
-        createContext (languageCode: string, templateData: object): void;
         t (languageCode?: string, resourceKey?: string, templateData?: object): string;
         t (resourceKey?: string, templateData?: object): string;
         locale (): string;
