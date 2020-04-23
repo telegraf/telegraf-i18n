7937c1ad0eab8bba2b4ab8fc685ad28f066c7e28
daremove <daremove1@gmail.com>
2020-04-23 23:46:27 +0500

    Add typings for match and reply functions

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,32 +1,38 @@
+import { ExtraReplyMessage } from "telegraf/typings/telegram-types";
+
 declare module 'telegraf-i18n' {
-    interface Config {
-        directory?: string;
-        useSession?: boolean;
-        sessionName?: string;
-        allowMissing?: boolean;
-        defaultLanguageOnMissing?: boolean;
-        defaultLanguage?: string;
-    }
+  interface Config {
+    directory?: string;
+    useSession?: boolean;
+    sessionName?: string;
+    allowMissing?: boolean;
+    defaultLanguageOnMissing?: boolean;
+    defaultLanguage?: string;
+  }
 
-    type ContextUpdate = (ctx: any, next?: (() => any) | undefined) => any;
+  type ContextUpdate = (ctx: any, next?: (() => any) | undefined) => any;
 
-    export class I18n {
-        constructor (input: Config);
-        loadLocales (directory: string): void;
-        loadLocale (languageCode: string, i18Data: object): void;
-        resetLocale (languageCode: string): void;
-        availableLocales (): string[];
-        resourceKeys (languageCode: string): string[];
-        missingKeys (languageOfInterest: string, referenceLanguage?: string): string[];
-        overspecifiedKeys (languageOfInterest: string, referenceLanguage?: string): string[];
-        translationProgress (languageOfInterest: string, referenceLanguage?: string): number;
-        middleware(): ContextUpdate;
-        createContext (languageCode: string, templateData: object): void;
-        t (languageCode?: string, resourceKey?: string, templateData?: object): string;
-        t (resourceKey?: string, templateData?: object): string;
-        locale (): string;
-        locale (languageCode?: string): void;
-    }
+  export class I18n {
+    constructor (input: Config);
+    loadLocales (directory: string): void;
+    loadLocale (languageCode: string, i18Data: object): void;
+    resetLocale (languageCode: string): void;
+    availableLocales (): string[];
+    resourceKeys (languageCode: string): string[];
+    missingKeys (languageOfInterest: string, referenceLanguage?: string): string[];
+    overspecifiedKeys (languageOfInterest: string, referenceLanguage?: string): string[];
+    translationProgress (languageOfInterest: string, referenceLanguage?: string): number;
+    middleware(): ContextUpdate;
+    createContext (languageCode: string, templateData: object): void;
+    t (languageCode?: string, resourceKey?: string, templateData?: object): string;
+    t (resourceKey?: string, templateData?: object): string;
+    locale (): string;
+    locale (languageCode?: string): void;
+  }
 
-    export default I18n;
+  export function match(resourceKey: string, templateData?: any): [string];
+
+  export function reply(resourceKey: string, extra?: ExtraReplyMessage): ContextUpdate;
+
+  export default I18n;
 }
