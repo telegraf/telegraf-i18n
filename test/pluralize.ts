8f9024d54562d080e2e3648062f3fd0ee04d27ac
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-10-07 23:02:22 +0200

    test: simpler to read template

--- test/pluralize.ts
+++ test/pluralize.ts
@@ -5,8 +5,8 @@ import {I18n} from '../source/i18n'
 test('can pluralize', t => {
   const i18n = new I18n()
   i18n.loadLocale('en', {
-    // eslint-disable-next-line no-template-curly-in-string
-    pluralize: '${pluralize(n, \'There was an apple\', \'There were apples\')}'
+    // eslint-disable-next-line no-template-curly-in-string, @typescript-eslint/quotes
+    pluralize: "${pluralize(n, 'There was an apple', 'There were apples')}"
   })
   t.is(i18n.t('en', 'pluralize', {n: 0}), '0 There were apples')
   t.is(i18n.t('en', 'pluralize', {n: 1}), '1 There was an apple')
@@ -16,8 +16,8 @@ test('can pluralize', t => {
 test('can pluralize using functional forms', t => {
   const i18n = new I18n()
   i18n.loadLocale('en', {
-    // eslint-disable-next-line no-template-curly-in-string
-    pluralize: '${pluralize(n, n => \'There was an apple\', n => \'There were \' + n + \' apples\')}'
+    // eslint-disable-next-line no-template-curly-in-string, @typescript-eslint/quotes
+    pluralize: "${pluralize(n, n => 'There was an apple', n => 'There were ' + n + ' apples')}"
   })
   t.is(i18n.t('en', 'pluralize', {n: 0}), 'There were 0 apples')
   t.is(i18n.t('en', 'pluralize', {n: 1}), 'There was an apple')
